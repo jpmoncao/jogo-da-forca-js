3826d74d4d0c59a5a7bb604c8b152be8ffd97c80
jpmoncao <pedrohjoao44@gmail.com>
2025-06-11 17:05:17 -0300

    jogo da forca terminado

--- script.js
+++ script.js
@@ -1,4 +1,154 @@
-const desenharForca = (erros = 0) => {
+const cardResultado = document.querySelector('#card-resultado');
+const cardMensagem = document.querySelector('#card-mensagem');
+
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext("2d");
+
+const temas = [
+    {
+        tema: "Esporte",
+        palavras: [
+            "FUTEBOL",
+            "BASQUETE",
+            "TENIS",
+            "NATACAO",
+            "CORRIDA",
+            "CICLISMO",
+            "VOLEI",
+            "BOXE",
+            "SURFE",
+            "GOLF"
+        ]
+    },
+    {
+        tema: "Animal",
+        palavras: [
+            "CACHORRO",
+            "GATO",
+            "LEAO",
+            "TIGRE",
+            "ELEFANTE",
+            "GIRAFA",
+            "MACACO",
+            "URSO",
+            "AGUIA",
+            "COBRA"
+        ]
+    },
+    {
+        tema: "Alimento",
+        palavras: [
+            "ARROZ",
+            "FEIJAO",
+            "CARNE",
+            "FRANGO",
+            "MACA",
+            "BANANA",
+            "PAO",
+            "LEITE",
+            "OVO",
+            "QUEIJO"
+        ]
+    },
+    {
+        tema: "País",
+        palavras: [
+            "BRASIL",
+            "CANADA",
+            "JAPAO",
+            "FRANCA",
+            "ALEMANHA",
+            "ITALIA",
+            "EGITO",
+            "CHINA",
+            "AUSTRALIA",
+            "MEXICO"
+        ]
+    },
+    {
+        tema: "Profissão",
+        palavras: [
+            "MEDICO",
+            "PROFESSOR",
+            "ENGENHEIRO",
+            "ADVOGADO",
+            "PROGRAMADOR",
+            "BOMBEIRO",
+            "POLICIAL",
+            "CANTOR",
+            "MOTORISTA",
+            "PADEIRO"
+        ]
+    }
+];
+
+const indexTema = Math.trunc((Math.random() * 10) / 2);
+const TEMA_ESCOLHIDA = temas[indexTema].tema;
+
+const indexPalavra = Math.trunc((Math.random() * 10));
+const PALAVRA_ESCOLHIDA = temas[indexTema].palavras[indexPalavra];
+
+const p = document.createElement('p');
+p.innerHTML = TEMA_ESCOLHIDA;
+cardResultado.appendChild(p);
+
+const letrasTentadas = [];
+const letrasErradas = [];
+const letrasCorretas = [];
+
+const mostrarMensagemErro = (mensagem) => {
+    cardMensagem.innerHTML = mensagem;
+}
+
+const mostrarGanhouJogo = () => {
+    cardResultado.innerHTML = '';
+
+    const h3 = document.createElement('h3');
+    h3.innerHTML = 'VOCÊ GANHOU!';
+    cardResultado.appendChild(h3);
+
+    const p = document.createElement('p');
+    p.innerHTML = 'Clique em "Tente novamente" para reiniciar o jogo';
+    cardResultado.appendChild(p);
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Tente novamente';
+    btn.onclick = () => location.reload();
+    cardResultado.appendChild(btn);
+
+    cardResultado.classList.add('sucesso');
+
+    document.querySelector('fieldset').disabled = true;
+}
+
+const mostrarPerdeuJogo = () => {
+    cardResultado.innerHTML = '';
+
+    const h3 = document.createElement('h3');
+    h3.innerHTML = 'ENFORCADO!';
+    cardResultado.appendChild(h3);
+
+    const p = document.createElement('p');
+    p.innerHTML = 'Clique em "Tente novamente" para reiniciar o jogo';
+    cardResultado.appendChild(p);
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Tente novamente';
+    btn.onclick = () => location.reload();
+    cardResultado.appendChild(btn);
+
+    cardResultado.classList.add('erro');
+
+    document.querySelector('fieldset').disabled = true;
+}
+
+const desenharForca = (letrasCorretas = [], letrasErradas = []) => {
+    console.log({ TEMA_ESCOLHIDA, PALAVRA_ESCOLHIDA })
+
+    const erros = letrasErradas.length;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     // Desenhando a forca
     ctx.fillRect(50, 50, 4, 200);
     ctx.fillRect(50, 50, 100, 4);
@@ -48,14 +198,47 @@ const desenharForca = (erros = 0) => {
         ctx.stroke();
     }
 
-    if (erros >= 7) {
-        alert('ENFORCADO! \nA palavra correta era ' + PALAVRA_CHAVE + '! Tente novamente.')
-    }
+    const LETRA_LEFT_INIT = 190;
+    const LETRA_WIDTH = 30;
+    const LETRA_PADDING = 4;
+    const FONT_STYLE = "20px sans-serif";
+
+    PALAVRA_ESCOLHIDA.split('').forEach((letra, index) => {
+        let x = 0;
+        let y = 248;
+
+        ctx.font = FONT_STYLE;
+        if (index > 0) {
+            x = (LETRA_LEFT_INIT + (LETRA_PADDING * index) + (index * LETRA_WIDTH));
+
+            if (letrasCorretas && letrasCorretas.indexOf(letra) > -1)
+                ctx.fillText(letra, x + 10, y - 1);
+        } else {
+            x = (LETRA_LEFT_INIT + (index * LETRA_WIDTH));
+
+            if (letrasCorretas && letrasCorretas.indexOf(letra) > -1)
+                ctx.fillText(letra, x + 10, y - 1);
+        }
+
+        ctx.fillRect(x, y, LETRA_WIDTH, 2);
+    });
+
+    letrasErradas.forEach((letra, index) => {
+        const x = 200;
+        const y = 50;
+
+        ctx.font = FONT_STYLE;
+        ctx.fillText(letra, x + (20 * index), y);
+    })
 }
 
 const tentarLetra = () => {
+    mostrarMensagemErro('');
+
     try {
         const letra = document.querySelector('#letra').value.toUpperCase();
+
+        document.querySelector('#letra').value = '';
 
         if (letra.trim() === '')
             throw new Error('Digite uma letra válida!');
@@ -68,26 +251,25 @@ const tentarLetra = () => {
 
         letrasTentadas.push(letra);
 
-        if (PALAVRA_CHAVE.indexOf(letra) > -1)
+        if (PALAVRA_ESCOLHIDA.indexOf(letra) > -1)
             letrasCorretas.push(letra);
         else
             letrasErradas.push(letra);
 
-        desenharForca(letrasErradas.length);
+        desenharForca(letrasCorretas, letrasErradas);
 
-        console.log({ PALAVRA_CHAVE, letra, letrasTentadas, letrasCorretas, letrasErradas });
+        if (PALAVRA_ESCOLHIDA.split('').every(item => letrasCorretas.includes(item)))
+            mostrarGanhouJogo();
+
+        if (letrasErradas.length >= 7)
+            mostrarPerdeuJogo();
+
+        console.log({ PALAVRA_ESCOLHIDA, letra, letrasTentadas, letrasCorretas, letrasErradas });
     } catch (error) {
+        mostrarMensagemErro(error.message)
         console.error(error);
     }
 }
 
-document.addEventListener('DOMContentLoaded', desenharForca);
+document.addEventListener('DOMContentLoaded', () => desenharForca());
 document.querySelector('#confirmar-letra').addEventListener('click', tentarLetra);
-
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext("2d");
-
-const PALAVRA_CHAVE = 'TESTE';
-const letrasTentadas = [];
-const letrasErradas = [];
-const letrasCorretas = [];
